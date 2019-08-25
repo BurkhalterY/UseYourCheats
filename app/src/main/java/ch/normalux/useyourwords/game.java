fc8f7be9704115f57a633df3b490d08000dbc946
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2019-08-25 13:34:40 +0200

    Added red color for House

--- app/src/main/java/ch/normalux/useyourwords/game.java
+++ app/src/main/java/ch/normalux/useyourwords/game.java
@@ -31,7 +31,19 @@ public class game extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        webView.loadUrl("javascript:(function(){ let inputs = document.getElementsByName('radio'); for(var i = 0; i < inputs.length; i++){ inputs[i].disabled = false; }})()");
+        webView.loadUrl(
+            "javascript:(function(){" +
+                "let inputs = document.getElementsByName('radio');" +
+                "for(var i = 0; i < inputs.length; i++){" +
+                    "inputs[i].disabled = false;" +
+                "}" +
+                "let labels = document.getElementsByTagName('label');" +
+                "for(var i = 0; i < labels.length; i++){" +
+                    "if(labels[i].htmlFor == 'HOUSE'){" +
+                        "labels[i].style.backgroundColor = 'coral';" +
+                    "}" +
+                "}" +
+            "})()");
         return true;
     }
 
